19c3962bfe9217c8d0378117cb47dd751e1fb883
Nikita Antonov <nikita45454@gmail.com>
2024-02-04 13:44:45 +0300

    feat: добавляет стандартный лэйаут

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout-default">
+    <HeaderPublic />
+    <slot />
+    <FooterPublic />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import HeaderPublic from '@/app-modules/layouts/header-public/index.vue';
+  import FooterPublic from '@/app-modules/layouts/footer-public/index.vue';
+</script>
+
+<style scoped lang="scss">
+  .layout-default {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+  }
+</style>
