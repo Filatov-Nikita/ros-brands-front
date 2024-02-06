72e6e550e63a8cd6f93018a12e2c8bdc125c5bd2
Nikita Antonov <nikita45454@gmail.com>
2024-02-06 22:04:01 +0300

    feat: добавляет базовый чекбокс

---
+++ components/Base/Checkbox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="checkbox-control" :class="{ 'checkbox-control--active': checked }" role="checkbox" @click="onClick">
+    <div class="checkbox-control__checkbox">
+      <BaseIcon class="checkbox-control__icon-active" name="checked" />
+    </div>
+    <span class="checkbox-control__label">Charuel</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = withDefaults(defineProps<{
+    modelValue?: any,
+    checkedValue?: any,
+    uncheckedValue?: any,
+  }>(), {});
+
+  const emit = defineEmits<{
+    (event: 'update:modelValue', val: any): void
+  }>();
+
+  const checked = computed(() => {
+    if(Array.isArray(props.modelValue)) {
+      return props.modelValue.includes(props.checkedValue);
+    }
+    return props.modelValue === props.checkedValue;
+  });
+
+  function onClick() {
+    if(Array.isArray(props.modelValue)) {
+      if(props.modelValue.includes(props.checkedValue)) {
+        const items = props.modelValue.filter(val => props.checkedValue !== val);
+        emit('update:modelValue', items);
+      } else {
+        emit('update:modelValue', [ ...props.modelValue, props.checkedValue ]);
+      }
+    } else {
+      if(props.modelValue === props.checkedValue) {
+        emit('update:modelValue', props.uncheckedValue);
+      } else {
+        emit('update:modelValue', props.checkedValue);
+      }
+    }
+  }
+</script>
+
+<style src="@/assets/css/components/checkbox.scss" lang="scss"></style>
