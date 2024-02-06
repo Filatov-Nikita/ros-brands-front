e538b524e365b1129041fd3221f35e900bac8ff5
Nikita Antonov <nikita45454@gmail.com>
2024-02-06 22:09:27 +0300

    feat: добавляет базовые кнопки

---
+++ components/Base/Button/Outline.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="base-button" :type="type">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+  withDefaults(defineProps<{
+    type?: 'button' | 'submit',
+  }>(), {
+    type: 'button',
+  });
+</script>
+
+<style scoped lang="scss">
+  .base-button {
+    border-radius: 10px;
+    text-align: center;
+    padding: 16px 20px;
+    border: 1px solid theme('colors.black');
+    @apply tw-text-black tw-font-medium tw-text-20 tw-leading-none;
+
+    &:hover {
+      background-color: #6A6A6A;
+      @apply tw-text-white;
+    }
+
+    &:active {
+      @apply tw-bg-black tw-text-white;
+    }
+  }
+</style>
---
+++ components/Base/Button/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="base-button" :type="type">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+  withDefaults(defineProps<{
+    type?: 'button' | 'submit',
+  }>(), {
+    type: 'button',
+  });
+</script>
+
+<style scoped lang="scss">
+  .base-button {
+    border-radius: 5px;
+    text-align: center;
+    padding: 11px 15px;
+    @apply tw-bg-black tw-text-white tw-font-medium;
+
+    &:hover {
+      background-color: #6A6A6A;
+    }
+
+    &:active {
+      @apply tw-bg-black;
+    }
+  }
+</style>
