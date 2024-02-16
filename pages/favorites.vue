44b2202a3aaa80ee922ee949fca2becae003c113
Nikita Antonov <nikita45454@gmail.com>
2024-02-16 23:44:43 +0300

    feat: создает страницу избранное

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,11 +1,23 @@
 <template>
-  <main class="page-paddings">
+  <main class="page-paddings favorite-page">
     <div class="wrapper">
-      fav
+      <h1 class="favorite-page__h1">Избранное</h1>
+      <FavoriteCatalog />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-
+  import FavoriteCatalog from '@/app-modules/favorite-catalog/index.vue';
 </script>
+
+<style scoped lang="scss">
+  .favorite-page {
+    &__h1 {
+      font-size: 32px;
+      line-height: normal;
+      margin-bottom: 30px;
+      @apply tw-font-bold;
+    }
+  }
+</style>
