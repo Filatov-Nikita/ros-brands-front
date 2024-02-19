1c20498b4b4386c4fccd86a72e76f88b04b9741b
Nikita Antonov <nikita45454@gmail.com>
2024-02-19 21:36:58 +0300

    feat: добавляет модуль образы от стилиста и встраивает на страницу

---
+++ app-modules/looks-with-designer/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="looks-with-designer" v-if="response && response.data.length > 0">
+    <h2 class="h2 looks-with-designer__h2">Образы</h2>
+    <LookList :looks="response.data" />
+
+    <div v-if="!pagination.isLast.value && meta" class="tw-mt-10 tw-flex tw-flex-col tw-items-center">
+      <BaseButtonOutline
+        class="tw-max-w-[250px] tw-w-full"
+        @click="loadMore"
+      >
+        Показать ещё
+      </BaseButtonOutline>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import type { LookListItem } from '@/types/looks';
+  import type { PaginateResponse } from '@/types/shared';
+  import { usePagination } from '@/composables/usePagination';
+  import usePaginateQuery from '@/composables/usePaginateQuery';
+  import LookList from '@/app-modules/look-catalog/components/Look/List.vue';
+
+  const props = defineProps<{
+    designerId: number,
+  }>();
+
+  const page = ref(1);
+
+  const { data: response, execute } = await useDataFetch<PaginateResponse<LookListItem[]>>(
+    computed(() => `designers/${props.designerId}/show-looks`),
+    {
+      query: computed(() => ({ page: page.value })),
+    }
+  );
+
+  const meta = computed(() => response.value?.meta ?? null);
+
+  const pagination = usePagination(meta, page);
+
+  const { loadMore } = usePaginateQuery(pagination, response, execute);
+</script>
+
+
+<style scoped lang="scss">
+  .looks-with-designer {
+    &__h2 {
+      margin-bottom: 20px;
+    }
+  }
+</style>
--- pages/designers/[id].vue
+++ pages/designers/[id].vue
@@ -2,10 +2,12 @@
   <main class="page-paddings">
     <div class="wrapper">
       <DesignerDetailed :id="+$route.params.id" />
+      <LookWithDesigner class="tw-mt-24" :designer-id="+$route.params.id" />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
   import DesignerDetailed from '@/app-modules/designer-detailed/index.vue';
+  import LookWithDesigner from '@/app-modules/looks-with-designer/index.vue';
 </script>
